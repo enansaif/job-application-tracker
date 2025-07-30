fc2eef9aa01a37cf3190841ff91082e804868e78
enansaif <enansaifme33@gmail.com>
2025-07-30 21:03:18 +0600

    added a django command to wait for database initialization

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,21 @@
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        connection = None
+        max_attempt = 10
+        while max_attempt > 0:
+            try:
+                connection = connections['default']
+                connection.cursor() # make connection attempt
+                self.stdout.write('Database initialized.')
+                return
+            except OperationalError:
+                self.stdout.write('Waiting 1 second for database to initialize...')
+                max_attempt -= 1
+                time.sleep(1)
+        self.stdout.write('Could not initialize database')
