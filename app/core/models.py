e8524401dd58cdeebe667eb82bdcd35accd2c6ab
enansaif <enansaifme33@gmail.com>
2025-07-31 16:00:31 +0600

    implemented the database design

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,14 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+APPLICATION_STATUS_CHOICES = [
+    ('applied', 'Applied'),
+    ('interviewing', 'Interviewing'),
+    ('rejected', 'Rejected'),
+    ('offer', 'Offer'),
+    ('accepted', 'Accepted'),
+]
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
@@ -36,7 +44,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(default=uuid4, unique=True, editable=False)
     email = models.EmailField(max_length=255, unique=True, validators=[validate_email])
-    name = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
@@ -44,3 +52,82 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Country(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    class Meta:
+        constraints = [
+        models.UniqueConstraint(fields=['user', 'name'], name='unique_user_country')
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    class Meta:
+        constraints = [
+        models.UniqueConstraint(fields=['user', 'name'], name='unique_user_tag')
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.SET_NULL)
+    link = models.URLField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
+    class Meta:
+        constraints = [
+        models.UniqueConstraint(fields=['user', 'name'], name='unique_user_company')
+    ]
+
+    def __str__(self):
+        return self.name
+
+
+class Resume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='resumes/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"Resume created @ {str(self.created_at)}"
+
+
+class Application(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
+    resume = models.ForeignKey(Resume, null=True, blank=True, on_delete=models.SET_NULL)
+    position = models.CharField(max_length=255)
+    link = models.URLField(null=True, blank=True)
+    note = models.TextField()
+    status = models.CharField(max_length=32, choices=APPLICATION_STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.position} @ {self.company.name}"
+
+
+class Interview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    date = models.DateField()
+    note = models.TextField()
+
+    def __str__(self):
+        return f"{self.application.company.name} on {str(self.date)}"
+
