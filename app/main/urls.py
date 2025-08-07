59226a796311670d66eaeb1f42800e0883e006c9
enansaif <enansaifme33@gmail.com>
2025-08-07 18:33:59 +0600

    changed api routes for tag

--- app/main/urls.py
+++ app/main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.TagListCreateView.as_view(), name='tags-list-create'),
-    path('<id>/', views.TagUpdateDestroyView.as_view(), name='tags-update-destroy')
+    path('tags/', views.TagListCreateView.as_view(), name='tags-list-create'),
+    path('tags/<id>/', views.TagUpdateDestroyView.as_view(), name='tags-update-destroy')
 ]
